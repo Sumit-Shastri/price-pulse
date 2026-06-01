f529340b9f4e752de8a88d00a65a9306fc77688c
Sumit-Shastri <shastrisumit25@gmail.com>
2026-06-01 17:11:25 +0530

    method declaration : utils.product_price_tracker()

---
+++ utils.py
@@ -0,0 +1,35 @@
+"""
+//////////////////////////////////////////////////////////////////////
+//  Imports
+//////////////////////////////////////////////////////////////////////
+"""
+
+
+"""
+//////////////////////////////////////////////////////////////////////
+//  Method name     : product_price_tracker()
+//  input           : price         -->    current price of product
+//  output          : csv file having columns : 1. srno
+//                                              2. date
+//                                              3. day
+//                                              4. time
+//                                              5. product price
+//                                              6. difference from target price
+//                                              7. target price
+// description      : This method accepts current product price from
+//                    price fetcher methods (eg. amazon_tracker) and
+//                    generates a csv file for the particular product
+//                    ,which has above given fields.This csv file can 
+//                    later used for price tracking and data visualization.
+// Author           : Sumit Shastri
+// Date             : 01/06/2026
+//////////////////////////////////////////////////////////////////////
+"""
+
+def product_price_tracker(price):
+    print("Coming soon")
+"""
+//////////////////////////////////////////////////////////////////////
+//  END
+//////////////////////////////////////////////////////////////////////
+"""
